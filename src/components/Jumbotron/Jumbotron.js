f5bd7e97c2d07f6acf42b13eae9a1bc2beb77ba8
Payton <greatpayton@gmail.com>
2021-02-14 18:31:40 -0700

    imported Container, Item, Inner styled components

--- src/components/Jumbotron/Jumbotron.js
+++ src/components/Jumbotron/Jumbotron.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Container, Item, Inner } from './styles/style';
 
 function Jumbotron({ children, direction = 'row', ...restProps }) {
   return (
