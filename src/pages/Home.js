38fdd08bb589c799f8d18fc4910934afd6f81a0f
Payton <greatpayton@gmail.com>
2021-02-14 18:29:49 -0700

    added Home page

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import JumboTron from '../components/Jumbotron/Jumbotron';
+
+function Home() {
+  return (
+    <JumboTron.Container>
+      <p>Hello</p>
+    </JumboTron.Container>
+  );
+}
+
+export default Home;
