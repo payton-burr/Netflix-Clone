955d71f56b19a4475373f0b214f967d78e8da73e
Payton <greatpayton@gmail.com>
2021-02-14 18:40:01 -0700

    styled SubTitle

--- src/components/Jumbotron/styles/style.js
+++ src/components/Jumbotron/styles/style.js
@@ -11,7 +11,15 @@ export const Container = styled.section``;
 
 export const Title = styled.h1``;
 
-export const SubTitle = styled.h2``;
+export const SubTitle = styled.h2`
+  font-size: 26px;
+  font-weight: normal;
+  line-height: normal;
+
+  @media (max-width: 600px) {
+    font-size: 18px;
+  }
+`;
 
 export const Image = styled.img`
   max-width: 100%;
