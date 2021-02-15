ebfad9fdd71745f660928a0f346a47d0b165268f
Payton <greatpayton@gmail.com>
2021-02-14 20:16:39 -0700

    moved Jumobotron from Home to JumbotronContainer in containers

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,8 @@
 import React from 'react';
-import Jumbotron from '../components/Jumbotron/Jumbotron';
+import JumbotronContainer from '../containers/JumbotronContainer';
 
 function Home() {
-  return (
-    <Jumbotron.Container>
-      <Jumbotron.Title>Hello</Jumbotron.Title>
-      <Jumbotron.SubTitle>Hello</Jumbotron.SubTitle>
-    </Jumbotron.Container>
-  );
+  return <JumbotronContainer />;
 }
 
 export default Home;
