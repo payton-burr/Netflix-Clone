0e7efed28c1b86b1ef3ee62e3d2e409aea7c4df6
Payton <greatpayton@gmail.com>
2021-02-14 17:54:22 -0700

    Jumbotron base component

---
+++ src/components/Jumbotron/Jumbotron.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Jumbotron({ children, direction = 'row', ...restProps }) {
+  return (
+    <Item {...restProps}>
+      <Inner direction={direction}>{children}</Inner>
+    </Item>
+  );
+}
+
+export default Jumbotron;
