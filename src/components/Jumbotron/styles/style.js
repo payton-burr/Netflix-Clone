53a123d055cbb84540c2e572f68797668b26478b
Payton <greatpayton@gmail.com>
2021-02-14 18:55:50 -0700

    more styles to Inner

--- src/components/Jumbotron/styles/style.js
+++ src/components/Jumbotron/styles/style.js
@@ -3,6 +3,15 @@ import styled from 'styled-components';
 export const Inner = styled.div`
   display: flex;
   align-items: center;
+  justify-content: space-between;
+  flex-direction: row;
+  max-width: 1100px;
+  margin: auto;
+  width: 100%;
+
+  @media (max-width: 1000px) {
+    flex-direction: column;
+  }
 `;
 
 export const Item = styled.div``;
