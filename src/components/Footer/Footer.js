2340568a366c5371dc64b9c9ffa8ad1e55974ff4
Payton <greatpayton@gmail.com>
2021-02-17 16:25:33 -0700

    added Footer.Row

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Container } from './styles/style';
+import { Container, Row } from './styles/style';
 
 function Footer({ children, ...restProps }) {
   return <Container {...restProps}>{children}</Container>;
 }
 
+Footer.Row = function FooterRow({ children, ...restProps }) {
+  return <Row {...restProps}>{children}</Row>;
+};
+
 Footer.propTypes = {
+  children: PropTypes.node,
+};
+
+Footer.Row.propTypes = {
   children: PropTypes.node,
 };
 
