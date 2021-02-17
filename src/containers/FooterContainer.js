2ffa9dbe72086170b588321ef4437d375e2651db
Payton <greatpayton@gmail.com>
2021-02-17 16:54:21 -0700

    added compound components to Footer

--- src/containers/FooterContainer.js
+++ src/containers/FooterContainer.js
@@ -4,7 +4,38 @@ import { Footer } from '../components';
 function FooterContainer() {
   return (
     <Footer>
-      <p>Hello</p>
+      <Footer.Title>Questions? Call 1-234-567-8910</Footer.Title>
+      <Footer.Break />
+      <Footer.Row>
+        <Footer.Column>
+          <Footer.Link href="#">FAQs</Footer.Link>
+          <Footer.Link href="#">Investor Relations</Footer.Link>
+          <Footer.Link href="#">Ways to Watch</Footer.Link>
+          <Footer.Link href="#">Coporate Information</Footer.Link>
+          <Footer.Link href="#">Netflix Clone Originals</Footer.Link>
+        </Footer.Column>
+
+        <Footer.Column>
+          <Footer.Link href="#">Help Center</Footer.Link>
+          <Footer.Link href="#">Jobs</Footer.Link>
+          <Footer.Link href="#">Terms of Use</Footer.Link>
+          <Footer.Link href="#">Contact Us</Footer.Link>
+        </Footer.Column>
+
+        <Footer.Column>
+          <Footer.Link href="#">Account</Footer.Link>
+          <Footer.Link href="#">Redeem Gift Cards</Footer.Link>
+          <Footer.Link href="#">Privacy</Footer.Link>
+          <Footer.Link href="#">Speed Test</Footer.Link>
+        </Footer.Column>
+
+        <Footer.Column>
+          <Footer.Link href="#">Media Center</Footer.Link>
+          <Footer.Link href="#">Buy Gift Cards</Footer.Link>
+          <Footer.Link href="#">Cookie Preferences</Footer.Link>
+          <Footer.Link href="#">Legal Notices</Footer.Link>
+        </Footer.Column>
+      </Footer.Row>
     </Footer>
   );
 }
