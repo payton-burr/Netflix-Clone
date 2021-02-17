11693dacb4eee3159be4d2745c1f90719831aedf
Payton <greatpayton@gmail.com>
2021-02-17 16:17:28 -0700

    FooterContainer markup

---
+++ src/containers/FooterContainer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Footer } from '../components';
+
+function FooterContainer() {
+  return (
+    <Footer>
+      <p>Hello</p>
+    </Footer>
+  );
+}
+
+export default FooterContainer;
