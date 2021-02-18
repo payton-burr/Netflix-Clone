a99ceade00f5aa1d88f36c194b6429300971f31d
Payton <greatpayton@gmail.com>
2021-02-18 16:59:31 -0700

    exported OptForm for tree shaking purposes

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { default as Jumbotron } from './Jumbotron/Jumbotron';
 export { default as Footer } from './Footer/Footer';
 export { default as Accordion } from './Accordion/Accordion';
+export { default as OptForm } from './Opt-form/OptForm';
