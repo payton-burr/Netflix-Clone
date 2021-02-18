5937a46fc2965f3933865a375107955ea41e7122
Payton <greatpayton@gmail.com>
2021-02-18 16:52:19 -0700

    modified OptForm.Button

--- src/components/Opt-form/OptForm.js
+++ src/components/Opt-form/OptForm.js
@@ -1,20 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Container, Input, Button } from './styles/style';
 
 function OptForm({ children, ...restProps }) {
   return <Container {...restProps}>{children}</Container>;
 }
 
 OptForm.Input = function OptFormInput({ ...restProps }) {
-  <Input {...restProps} />;
+  return <Input {...restProps} />;
 };
 
 OptForm.Button = function OptFormButton({ children, ...restProps }) {
-  <Button {...restProps}>{children}</Button>;
+  return (
+    <Button {...restProps}>
+      {children} <img src="/images/icons/chevron-right.png" alt="Try Now" />
+    </Button>
+  );
 };
 
 OptForm.propTypes = {
   children: PropTypes.node,
+};
+
+OptForm.Button.propTypes = {
+  children: PropTypes.string,
 };
 
 export default OptForm;
