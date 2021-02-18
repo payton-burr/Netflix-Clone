9f0ea8fcc1e328aef54de0bf948db3337f981dca
Payton <greatpayton@gmail.com>
2021-02-17 17:37:08 -0700

    added styles to Footer

--- src/components/Footer/styles/style.js
+++ src/components/Footer/styles/style.js
@@ -2,16 +2,38 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   display: flex;
+  max-width: 1000px;
+  flex-direction: column;
+  padding: 70px 56px;
+  margin: auto;
 `;
 
-export const Row = styled.div``;
+export const Row = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(230px, 1fr));
+`;
 
-export const Column = styled.div``;
+export const Column = styled.div`
+  display: flex;
+  flex-direction: column;
+  text-align: left;
+`;
 
-export const Link = styled.a``;
+export const Link = styled.a`
+  color: #757575;
+  margin-bottom: 20px;
+  font-size: 13px;
+  text-decoration: none;
+`;
 
-export const Title = styled.p``;
+export const Title = styled.p`
+  color: #757575;
+  margin-bottom: 30px;
+`;
 
 export const Button = styled.button``;
 
-export const Break = styled.div``;
+export const Break = styled.div`
+  flex-basis: 100%;
+  height: 10px;
+`;
