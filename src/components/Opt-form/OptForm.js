b809f3251aa58a09d8b4170df123f346631068e4
Payton <greatpayton@gmail.com>
2021-02-18 16:47:46 -0700

    added OptForm.Input and OptForm.Button

--- src/components/Opt-form/OptForm.js
+++ src/components/Opt-form/OptForm.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function OptForm() {
-  return <div>hello</div>;
+function OptForm({ children, ...restProps }) {
+  return <Container {...restProps}>{children}</Container>;
 }
+
+OptForm.Input = function OptFormInput({ ...restProps }) {
+  <Input {...restProps} />;
+};
+
+OptForm.Button = function OptFormButton({ children, ...restProps }) {
+  <Button {...restProps}>{children}</Button>;
+};
+
+OptForm.propTypes = {
+  children: PropTypes.node,
+};
 
 export default OptForm;
