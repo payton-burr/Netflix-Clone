1942a81928b896a90622710196cbacb2f2ee403e
Payton <greatpayton@gmail.com>
2021-02-17 19:05:29 -0700

    changed Accordian styled elements

--- src/components/Accordion/styles/style.js
+++ src/components/Accordion/styles/style.js
@@ -1,11 +1,11 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.section``;
 
 export const Inner = styled.div``;
 
-export const Title = styled.p``;
+export const Title = styled.h1``;
 
-export const Header = styled.p``;
+export const Header = styled.div``;
 
-export const Body = styled.p``;
+export const Body = styled.body``;
