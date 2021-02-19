8e777a3166479be0e38132ae8824ba5e52d07de9
Payton Burr <greatpayton@gmail.com>
2021-02-19 13:58:19 -0700

    added bg prop and changed Background to conditionally render

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Link as ReactRouterLink } from 'react-router-dom';
 import { Background } from './styles/style';
 
-function Header({ children, ...restProps }) {
-  return <Background>hello</Background>;
+function Header({ bg = true, children, ...restProps }) {
+  return bg ? (
+    <Background {...restProps}>{children}</Background>
+  ) : (
+    <React.Fragment {...restProps}>{children}</React.Fragment>
+  );
 }
+
+Header.propTypes = {
+  children: PropTypes.node,
+  bg: PropTypes.bool,
+};
 
 export default Header;
