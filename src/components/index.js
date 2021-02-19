5cef2e05110bebd7c6420796911fdb836d27af49
Payton Burr <greatpayton@gmail.com>
2021-02-19 14:21:53 -0700

    exported Header from index for tree shaking purposes

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@ export { default as Jumbotron } from './Jumbotron/Jumbotron';
 export { default as Footer } from './Footer/Footer';
 export { default as Accordion } from './Accordion/Accordion';
 export { default as OptForm } from './Opt-form/OptForm';
+export { default as Header } from './Header/Header';
