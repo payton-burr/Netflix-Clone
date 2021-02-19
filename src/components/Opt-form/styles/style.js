a312e04ba49398c2cff99c5e0671e76850f63034
Payton <greatpayton@gmail.com>
2021-02-18 17:20:32 -0700

    styled OptForm.Input

--- src/components/Opt-form/styles/style.js
+++ src/components/Opt-form/styles/style.js
@@ -13,7 +13,17 @@ export const Container = styled.div`
   }
 `;
 
-export const Input = styled.input``;
+// Modify padding below to 10px 10px 0
+// Change placeholder to a label to properly animate
+export const Input = styled.input`
+  max-width: 450px;
+  width: 100%;
+  border: 0;
+  padding: 10px;
+  height: 60px;
+  box-sizing: border-box;
+  font-size: 16px;
+`;
 
 export const Button = styled.button``;
 
