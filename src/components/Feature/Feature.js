0e30466acfe68b361a19f017391d314cf6f3f0c4
Payton <greatpayton@gmail.com>
2021-02-19 16:29:05 -0700

    added Title

--- src/components/Feature/Feature.js
+++ src/components/Feature/Feature.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Container } from './styles/style';
+import { Container, Title } from './styles/style';
 
 function Feature({ children, ...restProps }) {
   return <Container {...restProps}>{children}</Container>;
 }
 
+Feature.Title = function FeatureTitle({ children, ...restProps }) {
+  return <Title {...restProps}>{children}</Title>;
+};
+
 Feature.propTypes = {
   children: PropTypes.node,
+};
+
+Feature.Title.propTypes = {
+  children: PropTypes.string,
 };
 
 export default Feature;
--- src/components/Feature/styles/style.js
+++ src/components/Feature/styles/style.js
@@ -1,3 +1,5 @@
 import styled from 'styled-components';
 
 export const Container = styled.section``;
+
+export const Title = styled.h1``;
