aa9c9123f89f92bcb02124d192199b357e9d4e73
Payton Burr <greatpayton@gmail.com>
2021-02-19 14:18:25 -0700

    changed Header styled component elements

--- src/components/Header/styles/style.js
+++ src/components/Header/styles/style.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
-export const Background = styled.div``;
+export const Background = styled.section``;
 
 export const Container = styled.div``;
 
-export const Logo = styled.div``;
+export const Logo = styled.p``;
