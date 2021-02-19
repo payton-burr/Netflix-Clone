0e0cfffd0707892b57d0955e9beca51edd8d2c76
Payton Burr <greatpayton@gmail.com>
2021-02-19 14:45:23 -0700

    Header styles

--- src/components/Header/styles/style.js
+++ src/components/Header/styles/style.js
@@ -1,12 +1,24 @@
 import styled from 'styled-components';
 import { Link as ReactRouterLink } from 'react-router-dom';
 
-export const Background = styled.section``;
+export const Background = styled.section`
+  display: flex;
+  flex-direction: column;
+`;
 
 export const Container = styled.div``;
 
 export const Link = styled.p``;
 
-export const Logo = styled.img``;
+export const Logo = styled.img`
+  height: 32px;
+  width: 108px;
+  margin-right: 40px;
+
+  @media (min-width: 1449px) {
+    height: 45px;
+    width: 167px;
+  }
+`;
 
 export const ButtonLink = styled(ReactRouterLink)``;
