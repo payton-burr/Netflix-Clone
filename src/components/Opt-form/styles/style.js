9166c68a27eb4aa2afb4d545ab50d46a004c2357
Payton <greatpayton@gmail.com>
2021-02-18 17:49:29 -0700

    more OptForm styled to Button and Text

--- src/components/Opt-form/styles/style.js
+++ src/components/Opt-form/styles/style.js
@@ -36,13 +36,39 @@ export const Button = styled.button`
   font-size: 26px;
   border: 0;
   cursor: pointer;
+  border-radius: 2px;
 
   img {
     margin-left: 10px;
     filter: brightness(0) invert(1);
     width: 10px;
     height: 20px;
+
+    @media (max-width: 1000px) {
+      width: 6px;
+      height: 12px;
+    }
+  }
+
+  &:hover {
+    background: #f40612;
+  }
+
+  @media (max-width: 1000px) {
+    height: 40px;
+    font-size: 16px;
+    width: auto;
+    padding: 0 16px;
+    margin-top: 1em;
   }
 `;
 
-export const Text = styled.p``;
+export const Text = styled.p`
+  font-size: 20px;
+  margin: 0 3.65em;
+
+  @media (max-width: 1000px) {
+    padding: 6px;
+    margin: 0;
+  }
+`;
