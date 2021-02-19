88af3fd9d82ec5e7c7139f907ef5b0c8c0de6db8
Payton Burr <greatpayton@gmail.com>
2021-02-19 14:20:25 -0700

    imported react-router to use with ButtonLink

--- src/components/Header/styles/style.js
+++ src/components/Header/styles/style.js
@@ -1,7 +1,10 @@
 import styled from 'styled-components';
+import { Link as ReactRouterLink } from 'react-router-dom';
 
 export const Background = styled.section``;
 
 export const Container = styled.div``;
 
 export const Logo = styled.p``;
+
+export const ButtonLink = styled(ReactRouterLink)``;
