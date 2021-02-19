717785ba37d15116bb1851e348d192a9bac3e33a
Payton <greatpayton@gmail.com>
2021-02-19 16:31:47 -0700

    added Feature.SubTitle

--- src/components/Feature/Feature.js
+++ src/components/Feature/Feature.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Container, Title } from './styles/style';
+import { Container, Title, SubTitle } from './styles/style';
 
 function Feature({ children, ...restProps }) {
   return <Container {...restProps}>{children}</Container>;
@@ -10,11 +10,19 @@ Feature.Title = function FeatureTitle({ children, ...restProps }) {
   return <Title {...restProps}>{children}</Title>;
 };
 
+Feature.SubTitle = function FeatureSubTitle({ children, ...restProps }) {
+  return <SubTitle {...restProps}>{children}</SubTitle>;
+};
+
 Feature.propTypes = {
   children: PropTypes.node,
 };
 
 Feature.Title.propTypes = {
+  children: PropTypes.string,
+};
+
+Feature.SubTitle.propTypes = {
   children: PropTypes.string,
 };
 
--- src/components/Feature/styles/style.js
+++ src/components/Feature/styles/style.js
@@ -3,3 +3,5 @@ import styled from 'styled-components';
 export const Container = styled.section``;
 
 export const Title = styled.h1``;
+
+export const SubTitle = styled.h3``;
