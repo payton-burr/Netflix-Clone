c8fd844bbcb1c20f01e31583d2024bf717ce678e
Payton Burr <greatpayton@gmail.com>
2021-02-19 14:42:41 -0700

    imported HeaderContainer into Home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,10 +2,14 @@ import React from 'react';
 import JumbotronContainer from '../containers/JumbotronContainer';
 import FooterContainer from '../containers/FooterContainer';
 import FaqsContainer from '../containers/FaqsContainer';
+import HeaderContainer from '../containers/HeaderContainer';
 
 function Home() {
   return (
     <>
+      <HeaderContainer>
+        <p>Hello</p>
+      </HeaderContainer>
       <JumbotronContainer />
       <FaqsContainer />
       <FooterContainer />
