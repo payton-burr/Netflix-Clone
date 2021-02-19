5e3a2d9aac2b4525760b6521f8c36c63c5ebe90e
Payton <greatpayton@gmail.com>
2021-02-18 17:11:59 -0700

    styled OptForm.Container

--- src/components/Opt-form/styles/style.js
+++ src/components/Opt-form/styles/style.js
@@ -1,6 +1,17 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  height: 100%;
+  margin-top 50px;
+  flex-wrap: wrap;
+
+  @media (max-width: 1000px) {
+      flex-direction: column;
+      align-items: center;
+  }
+`;
 
 export const Input = styled.input``;
 
