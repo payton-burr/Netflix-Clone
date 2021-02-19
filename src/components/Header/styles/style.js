11d76f72a2fa829b587beba0238c5d71e06054bb
Payton Burr <greatpayton@gmail.com>
2021-02-19 15:20:27 -0700

    Background and Container styles

--- src/components/Header/styles/style.js
+++ src/components/Header/styles/style.js
@@ -1,12 +1,33 @@
+/* eslint-disable prettier/prettier */
 import styled from 'styled-components';
 import { Link as ReactRouterLink } from 'react-router-dom';
 
 export const Background = styled.section`
   display: flex;
   flex-direction: column;
+  background url(${({ src }) =>
+    src
+      ? `/images/misc/${src}.jpg`
+      : `images/misc/home-bg.jpg`}) top left / cover no-repeat;
+
+@media (max-width: 1100px) {
+    ${({ dontShowOnSmallViewPort }) =>
+      dontShowOnSmallViewPort && `background: none;`}
+}
 `;
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  display: flex;
+  margin: 0 56px;
+  height: 64px;
+  padding: 18px 0;
+  justify-content: space-between;
+  align-items: center;
+
+  a {
+    display: flex;
+  }
+`;
 
 export const Link = styled.p``;
 
