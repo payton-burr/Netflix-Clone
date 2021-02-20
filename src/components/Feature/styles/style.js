fb4a12c4137e32935e6bd1b6f12105dda2dbedb3
Payton <greatpayton@gmail.com>
2021-02-19 18:38:28 -0700

    styled Container

--- src/components/Feature/styles/style.js
+++ src/components/Feature/styles/style.js
@@ -1,6 +1,12 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  border-bottom: 8px solid #222222;
+  text-align: center;
+  padding: 165px 45px;
+`;
 
 export const Title = styled.h1``;
 
