0da1346a35e319c4a09ed8c998ba7b5ab364e086
Payton <greatpayton@gmail.com>
2021-02-23 21:04:58 -0700

    added Form.Title

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,8 +1,12 @@
 import React from 'react';
-import { Container } from './styles/style';
+import { Container, Title } from './styles/style';
 
 function Form(children, ...restProps) {
   return <Container {...restProps}>{children}</Container>;
 }
+
+Form.Title = function FormTitle(children, ...restProps) {
+  return <Title {...restProps}>{children}</Title>;
+};
 
 export default Form;
