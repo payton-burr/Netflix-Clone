6090e3935a5121f4eb8c8ae9e37f55db7f9d0916
Payton <greatpayton@gmail.com>
2021-02-23 21:37:56 -0700

    deconstructed children, ...restProps

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -12,39 +12,39 @@ import {
   Submit,
 } from './styles/style';
 
-function Form(children, ...restProps) {
+function Form({ children, ...restProps }) {
   return <Container {...restProps}>{children}</Container>;
 }
 
-Form.Base = function FormBase(children, ...restProps) {
+Form.Base = function FormBase({ children, ...restProps }) {
   return <Base {...restProps}>{children}</Base>;
 };
 
-Form.Title = function FormTitle(children, ...restProps) {
+Form.Title = function FormTitle({ children, ...restProps }) {
   return <Title {...restProps}>{children}</Title>;
 };
 
-Form.Text = function FormText(children, ...restProps) {
+Form.Text = function FormText({ children, ...restProps }) {
   return <Text {...restProps}>{children}</Text>;
 };
 
-Form.TextSmall = function FormTextSmall(children, ...restProps) {
+Form.TextSmall = function FormTextSmall({ children, ...restProps }) {
   return <TextSmall {...restProps}>{children}</TextSmall>;
 };
 
-Form.Link = function FormLink(children, ...restProps) {
+Form.Link = function FormLink({ children, ...restProps }) {
   return <Link {...restProps}>{children}</Link>;
 };
 
-Form.Input = function FormInput(children, ...restProps) {
+Form.Input = function FormInput({ children, ...restProps }) {
   return <Input {...restProps}>{children}</Input>;
 };
 
-Form.Submit = function FormSubmit(children, ...restProps) {
+Form.Submit = function FormSubmit({ children, ...restProps }) {
   return <Submit {...restProps}>{children}</Submit>;
 };
 
-Form.Error = function FormError(children, ...restProps) {
+Form.Error = function FormError({ children, ...restProps }) {
   return <Error {...restProps}>{children}</Error>;
 };
 
