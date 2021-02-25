2f8d28d3c3962ebdc79ea462cdbc3bdf80e4f3de
Payton <greatpayton@gmail.com>
2021-02-24 17:24:21 -0700

    Form page markup

--- src/pages/Signin.js
+++ src/pages/Signin.js
@@ -1,14 +1,51 @@
+/* eslint-disable prettier/prettier */
 import React, { useState } from 'react';
 import HeaderContainer from '../containers/HeaderContainer';
 import { Form } from '../components';
 
 function Signin() {
   const [error, setError] = useState('');
+  const [emailAddress, setEmailAddress] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSignin = (event) => {
+    event.preventDefault();
+
+    // Call firebase to authenticate user
+    // If there's and error, setError state
+  };
   return (
     <HeaderContainer>
       <Form>
         <Form.Title>Sign In</Form.Title>
         {error && <Form.Error>{error}</Form.Error>}
+
+        <Form.Base onSubmit={handleSignin} method="POST">
+          <Form.Input
+            placeholder="Email address"
+            value={emailAddress}
+            onChange={({ target }) => setEmailAddress(target.value)}
+          />
+          <Form.Input
+            type="password"
+            placeholder="Password"
+            autoComplete="off"
+            value={password}
+            onChange={({ target }) => setPassword(target.value)}
+          />
+
+          <Form.Submit disabled={false} type="submit">
+            Sign In
+          </Form.Submit>
+
+          <Form.Text>
+            New to Netflix? <Form.Link>Sign up now.</Form.Link>
+          </Form.Text>
+
+          <Form.TextSmall>
+            This page is protected by Google reCAPTCHA
+          </Form.TextSmall>
+        </Form.Base>
       </Form>
     </HeaderContainer>
   );
