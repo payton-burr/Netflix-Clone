07f807780f2db1fda7737487f9c404a47605cbda
Payton <greatpayton@gmail.com>
2021-02-24 17:51:22 -0700

    Form styles

--- src/components/Form/styles/style.js
+++ src/components/Form/styles/style.js
@@ -1,20 +1,91 @@
 import styled from 'styled-components';
 import { Link as ReactRouterLink } from 'react-router-dom';
 
-export const Container = styled.section``;
+export const Container = styled.section`
+  display: flex;
+  flex-direction: column;
+  min-height: 660px;
+  background-color: rgba(0, 0, 0, 0.75);
+  border-radius: 5px;
+  box-sizing: border-box;
+  width: 100%;
+  margin: auto;
+  max-width: 450px;
+  padding: 60px 68px 40px;
+  margin-bottom: 100px;
+`;
 
-export const Base = styled.form``;
+export const Base = styled.form`
+  display: flex;
+  flex-direction: column;
+  max-width: 450px;
+  width: 100%;
+`;
 
-export const Title = styled.h1``;
+export const Title = styled.h1`
+  color: #ffffff;
+  margin-bottom: 28px;
+  margin-top: 0;
+`;
 
-export const Text = styled.h1``;
+export const Text = styled.h1`
+  color: #737373;
+  font-size: 16px;
+  font-weight: 500;
+`;
 
-export const TextSmall = styled.h2``;
+export const TextSmall = styled.h2`
+  margin-top: 10px;
+  font-size: 13px;
+  line-height: normal;
+  color: #8c8c8c;
+`;
 
-export const Link = styled(ReactRouterLink)``;
+export const Link = styled(ReactRouterLink)`
+  color: #ffffff;
+  text-decoration: none;
 
-export const Input = styled.input``;
+  &:hover {
+    text-decoration: underline;
+  }
+`;
 
-export const Submit = styled.button``;
+export const Input = styled.input`
+  background: #333333;
+  border-radius: 4px;
+  border: 0;
+  color: #ffffff;
+  height: 50px;
+  line-height: 50px;
+  padding-left: 20px;
+  margin-bottom: 20px;
 
-export const Error = styled.div``;
+  &:last-of-type {
+    margin-bottom: 30px;
+  }
+`;
+
+export const Submit = styled.button`
+  background: #e50914;
+  border-radius: 4px;
+  font-size: 16px;
+  font-weight: bold;
+  margin: 10px 0 12px;
+  padding: 16px;
+  border: 0;
+  color: #ffffff;
+  cursor: pointer;
+
+  &:disabled {
+    opacity: 0.5;
+  }
+`;
+
+export const Error = styled.div`
+  background: #e87c03;
+  border-radius: 4px;
+  font-size: 14px;
+  margin: 0 0 16px;
+  color: #ffffff;
+  padding: 15px 20px;
+`;
