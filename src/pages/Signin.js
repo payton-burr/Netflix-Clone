7f062a00123742b77a876765807c0bcb8d10b238
Payton <greatpayton@gmail.com>
2021-02-24 18:13:15 -0700

    imported FooterContainer

--- src/pages/Signin.js
+++ src/pages/Signin.js
@@ -2,6 +2,7 @@
 import React, { useState } from 'react';
 import HeaderContainer from '../containers/HeaderContainer';
 import { Form } from '../components';
+import FooterContainer from '../containers/FooterContainer';
 
 function Signin() {
   const [error, setError] = useState('');
@@ -17,39 +18,42 @@ function Signin() {
     // If there's and error, setError state
   };
   return (
-    <HeaderContainer>
-      <Form>
-        <Form.Title>Sign In</Form.Title>
-        {error && <Form.Error>{error}</Form.Error>}
+    <>
+      <HeaderContainer>
+        <Form>
+          <Form.Title>Sign In</Form.Title>
+          {error && <Form.Error>{error}</Form.Error>}
 
-        <Form.Base onSubmit={handleSignin} method="POST">
-          <Form.Input
-            placeholder="Email address"
-            value={emailAddress}
-            onChange={({ target }) => setEmailAddress(target.value)}
-          />
-          <Form.Input
-            type="password"
-            placeholder="Password"
-            autoComplete="off"
-            value={password}
-            onChange={({ target }) => setPassword(target.value)}
-          />
+          <Form.Base onSubmit={handleSignin} method="POST">
+            <Form.Input
+              placeholder="Email address"
+              value={emailAddress}
+              onChange={({ target }) => setEmailAddress(target.value)}
+            />
+            <Form.Input
+              type="password"
+              placeholder="Password"
+              autoComplete="off"
+              value={password}
+              onChange={({ target }) => setPassword(target.value)}
+            />
 
-          <Form.Submit disabled={isInvalid} type="submit">
-            Sign In
-          </Form.Submit>
+            <Form.Submit disabled={isInvalid} type="submit">
+              Sign In
+            </Form.Submit>
 
-          <Form.Text>
-            New to Netflix? <Form.Link>Sign up now.</Form.Link>
-          </Form.Text>
+            <Form.Text>
+              New to Netflix? <Form.Link>Sign up now.</Form.Link>
+            </Form.Text>
 
-          <Form.TextSmall>
-            This page is protected by Google reCAPTCHA
-          </Form.TextSmall>
-        </Form.Base>
-      </Form>
-    </HeaderContainer>
+            <Form.TextSmall>
+              This page is protected by Google reCAPTCHA
+            </Form.TextSmall>
+          </Form.Base>
+        </Form>
+      </HeaderContainer>
+      <FooterContainer />
+    </>
   );
 }
 
