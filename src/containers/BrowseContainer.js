a2b16bcfa35880167efc94a03840355b327899bf
Payton <greatpayton@gmail.com>
2021-02-25 23:08:46 -0700

    imported FooterContainer

--- src/containers/BrowseContainer.js
+++ src/containers/BrowseContainer.js
@@ -2,10 +2,16 @@ import React from 'react';
 import { Header } from '../components';
 import * as ROUTES from '../constants/routes';
 import FirebaseContext from '../context/firebase';
-// import SelectProfileContainer from './profiles';
+import { SelectProfileContainer } from './SelectProfileContainer';
+import { FooterContainer } from './FooterContainer';
 
 function BrowseContainer() {
-  return <div>Browse Container</div>;
+  return (
+    <>
+      <div>Browse Container</div>
+      <FooterContainer />
+    </>
+  );
 }
 
 export default BrowseContainer;
