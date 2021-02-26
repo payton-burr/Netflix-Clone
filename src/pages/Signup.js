72ec0e467c30449884628fd1e3ea4d0beafb7f82
Payton <greatpayton@gmail.com>
2021-02-25 22:27:35 -0700

    built Form for Signup page

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,8 +1,59 @@
-import React from 'react';
+/* eslint-disable prettier/prettier */
+import React, { useState } from 'react';
 import HeaderContainer from '../containers/HeaderContainer';
+import { Form } from '../components';
+import * as ROUTES from '../constants/routes';
 
 function Signup() {
-  return <HeaderContainer>Hello</HeaderContainer>;
+  const [firstName, setFirstName] = useState('');
+  const [emailAddress, setEmailAddress] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  return (
+    <HeaderContainer>
+      <Form>
+        <Form.Title>Sign Up</Form.Title>
+        {error && <Form.Error>{error}</Form.Error>}
+
+        <Form.Base method="POST">
+          <Form.Input
+            placeholder="First Name"
+            value={firstName}
+            onChange={({ target }) => setFirstName(target.value)}
+          />
+
+          <Form.Input
+            type="email"
+            placeholder="Email"
+            autocomplete="off"
+            value={emailAddress}
+            onChange={({ target }) => setEmailAddress(target.value)}
+          />
+
+          <Form.Input
+            type="password"
+            placeholder="Password"
+            autocomplete="off"
+            value={password}
+            onChange={({ target }) => setPassword(target.value)}
+          />
+
+          <Form.Submit type="submit" disabled>
+            Sign Up
+          </Form.Submit>
+
+          <Form.Text>
+            Already a user? <Form.Link to={ROUTES.SIGN_IN}>Sign in</Form.Link>
+          </Form.Text>
+
+          <Form.TextSmall>
+            This page is protected by Google reCAPTCHA
+          </Form.TextSmall>
+        </Form.Base>
+      </Form>
+    </HeaderContainer>
+  );
 }
 
 export default Signup;
