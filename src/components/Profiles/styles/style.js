b84a2109f54a434b7d29ebfa0f0b088cf7aad8af
Payton <greatpayton@gmail.com>
2021-02-25 23:56:42 -0700

    modified Profile styled component elements

--- src/components/Profiles/styles/style.js
+++ src/components/Profiles/styles/style.js
@@ -4,10 +4,10 @@ export const Container = styled.div``;
 
 export const Title = styled.h1``;
 
-export const List = styled.div``;
+export const List = styled.ul``;
 
-export const User = styled.div``;
+export const User = styled.li``;
 
 export const Image = styled.img``;
 
-export const Name = styled.h2``;
+export const Name = styled.p``;
