771747c9e9598f04af6814e3cfa11b86f19c5818
Payton <greatpayton@gmail.com>
2021-02-25 23:35:26 -0700

    SelectProfileContainer markup

--- src/containers/SelectProfileContainer.js
+++ src/containers/SelectProfileContainer.js
@@ -1,6 +1,38 @@
+/* eslint-disable prettier/prettier */
 /* eslint-disable import/prefer-default-export */
 import React from 'react';
+import { Header } from '../components';
+import * as ROUTES from '../constants/routes';
 
 export function SelectProfileContainer() {
-  return <div>Profile Container</div>;
+  return (
+    <>
+      <Header bg={false}>
+        <Header.Container>
+          <Header.Logo
+            to={ROUTES.HOME}
+            src="/images/misc/logo.svg"
+            alt="Netflix"
+          />
+        </Header.Container>
+      </Header>
+
+      <Profiles>
+        <Profiles.Title>Who's Watching?</Profiles.Title>
+        <Profiles.List>
+          <Profiles.User
+            onClick={() =>
+              setProfile({
+                displayName: user.displayName,
+                photoURL: user.photoURL,
+              })
+            }
+          >
+            <Profiles.Picture src={user.photoURL} />
+            <Profiles.Name>user.displayName</Profiles.Name>
+          </Profiles.User>
+        </Profiles.List>
+      </Profiles>
+    </>
+  );
 }
