f2c67cc45ed0b4bd2919cb58ae0deefe3bcdc664
Payton <greatpayton@gmail.com>
2021-02-25 22:29:35 -0700

    imported FooterContainer

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -2,6 +2,7 @@
 import React, { useState } from 'react';
 import HeaderContainer from '../containers/HeaderContainer';
 import { Form } from '../components';
+import FooterContainer from '../containers/FooterContainer';
 import * as ROUTES from '../constants/routes';
 
 function Signup() {
@@ -13,48 +14,51 @@ function Signup() {
   const isInvalid = firstName === '' || password === '' || emailAddress === '';
 
   return (
-    <HeaderContainer>
-      <Form>
-        <Form.Title>Sign Up</Form.Title>
-        {error && <Form.Error>{error}</Form.Error>}
+    <>
+      <HeaderContainer>
+        <Form>
+          <Form.Title>Sign Up</Form.Title>
+          {error && <Form.Error>{error}</Form.Error>}
 
-        <Form.Base method="POST">
-          <Form.Input
-            placeholder="First Name"
-            value={firstName}
-            onChange={({ target }) => setFirstName(target.value)}
-          />
+          <Form.Base method="POST">
+            <Form.Input
+              placeholder="First Name"
+              value={firstName}
+              onChange={({ target }) => setFirstName(target.value)}
+            />
 
-          <Form.Input
-            type="email"
-            placeholder="Email"
-            autocomplete="off"
-            value={emailAddress}
-            onChange={({ target }) => setEmailAddress(target.value)}
-          />
+            <Form.Input
+              type="email"
+              placeholder="Email"
+              autocomplete="off"
+              value={emailAddress}
+              onChange={({ target }) => setEmailAddress(target.value)}
+            />
 
-          <Form.Input
-            type="password"
-            placeholder="Password"
-            autocomplete="off"
-            value={password}
-            onChange={({ target }) => setPassword(target.value)}
-          />
+            <Form.Input
+              type="password"
+              placeholder="Password"
+              autocomplete="off"
+              value={password}
+              onChange={({ target }) => setPassword(target.value)}
+            />
 
-          <Form.Submit type="submit" disabled={isInvalid}>
-            Sign Up
-          </Form.Submit>
+            <Form.Submit type="submit" disabled={isInvalid}>
+              Sign Up
+            </Form.Submit>
 
-          <Form.Text>
-            Already a user? <Form.Link to={ROUTES.SIGN_IN}>Sign in</Form.Link>
-          </Form.Text>
+            <Form.Text>
+              Already a user? <Form.Link to={ROUTES.SIGN_IN}>Sign in</Form.Link>
+            </Form.Text>
 
-          <Form.TextSmall>
-            This page is protected by Google reCAPTCHA
-          </Form.TextSmall>
-        </Form.Base>
-      </Form>
-    </HeaderContainer>
+            <Form.TextSmall>
+              This page is protected by Google reCAPTCHA
+            </Form.TextSmall>
+          </Form.Base>
+        </Form>
+      </HeaderContainer>
+      <FooterContainer />
+    </>
   );
 }
 
