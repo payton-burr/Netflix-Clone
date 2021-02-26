8964133c88777925c2931c84977638a3ccfeb5d8
Payton <greatpayton@gmail.com>
2021-02-25 23:14:11 -0700

    set BrowseContainer to conditionally render

--- src/containers/BrowseContainer.js
+++ src/containers/BrowseContainer.js
@@ -6,11 +6,18 @@ import { SelectProfileContainer } from './SelectProfileContainer';
 import { FooterContainer } from './FooterContainer';
 
 function BrowseContainer() {
-  return (
+  const profile = {
+    displayName: 'Payton',
+    photoURL: '1',
+  };
+
+  return profile.displayName ? (
     <>
       <div>Browse Container</div>
       <FooterContainer />
     </>
+  ) : (
+    <SelectProfileContainer />
   );
 }
 
