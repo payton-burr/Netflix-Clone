30091dfcf5ef07b5ce374c8a52a36d343a5f33af
Payton <greatpayton@gmail.com>
2021-02-25 21:54:10 -0700

    added Signup

---
+++ src/pages/Signup.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import HeaderContainer from '../containers/HeaderContainer';
+
+function Signup() {
+  return <HeaderContainer>Hello</HeaderContainer>;
+}
+
+export default Signup;
