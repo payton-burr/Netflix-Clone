1a5936afb487ffbbde578db041ea8fc23dd8d296
Payton <greatpayton@gmail.com>
2021-02-25 23:45:46 -0700

    added Profiles.List

--- src/components/Profiles/Profiles.js
+++ src/components/Profiles/Profiles.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Container, Title } from './styles/style';
+import { Container, Title, List } from './styles/style';
 
 function Profiles({ children, ...restProps }) {
   return <Container {...restProps}>{children}</Container>;
@@ -10,12 +10,20 @@ Profiles.Title = function ProfilesTitle({ children, ...restProps }) {
   return <Title {...restProps}>{children}</Title>;
 };
 
+Profiles.List = function ProfilesList({ children, ...restProps }) {
+  return <List {...restProps}>{children}</List>;
+};
+
 Profiles.propTypes = {
   children: PropTypes.node,
 };
 
 Profiles.Title.propTypes = {
   children: PropTypes.string,
+};
+
+Profiles.List.propTypes = {
+  children: PropTypes.node,
 };
 
 export default Profiles;
