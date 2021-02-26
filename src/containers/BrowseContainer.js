9fb828c0801b93e7244512d1a7895471b547da3f
Payton <greatpayton@gmail.com>
2021-02-25 23:00:23 -0700

    imported necessary tools into BrowseContainer

--- src/containers/BrowseContainer.js
+++ src/containers/BrowseContainer.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import { Header } from '../components';
+import * as ROUTES from '../constants/routes';
+import FirebaseContext from '../context/firebase';
+// import SelectProfileContainer from './profiles';
 
 function BrowseContainer() {
   return <div>Browse Container</div>;
