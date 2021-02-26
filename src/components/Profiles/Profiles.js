e6dfdfce40c8851e06f58e7cd46d4ea960ce36f3
Payton <greatpayton@gmail.com>
2021-02-25 23:43:21 -0700

    added Profiles.Title

--- src/components/Profiles/Profiles.js
+++ src/components/Profiles/Profiles.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Container } from './styles/style';
+import { Container, Title } from './styles/style';
 
 function Profiles({ children, ...restProps }) {
   return <Container {...restProps}>{children}</Container>;
 }
 
+Profiles.Title = function ProfilesTitle({ children, ...restProps }) {
+  return <Title {...restProps}>{children}</Title>;
+};
+
 Profiles.propTypes = {
   children: PropTypes.node,
+};
+
+Profiles.Title.propTypes = {
+  children: PropTypes.string,
 };
 
 export default Profiles;
