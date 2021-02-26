e3465815320d5392c99821b8704e34d3bc9e9542
Payton <greatpayton@gmail.com>
2021-02-25 23:41:32 -0700

    added Profiles markup

--- src/components/Profiles/Profiles.js
+++ src/components/Profiles/Profiles.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { Container } from './styles/style';
 
-function Profiles() {
-  return <div>Hello</div>;
+function Profiles({ children, ...restProps }) {
+  return <Container {...restProps}>{children}</Container>;
 }
+
+Profiles.propTypes = {
+  children: PropTypes.node,
+};
 
 export default Profiles;
