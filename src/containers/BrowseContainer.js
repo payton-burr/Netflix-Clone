aaeb95ab62001747d505b587df5490b73d969473
Payton <greatpayton@gmail.com>
2021-02-27 13:40:17 -0700

    added BrowseContainer markup

--- src/containers/BrowseContainer.js
+++ src/containers/BrowseContainer.js
@@ -1,12 +1,17 @@
-import React, { useState } from 'react';
+/* eslint-disable prettier/prettier */
+import React, { useState, useContext } from 'react';
 import { Header } from '../components';
-import * as ROUTES from '../constants/routes';
 import FirebaseContext from '../context/firebase';
 import { SelectProfileContainer } from './SelectProfileContainer';
 import { FooterContainer } from './FooterContainer';
+import * as ROUTES from '../constants/routes';
 
 function BrowseContainer() {
+  const [category, setCategory] = useState('series');
   const [profile, setProfile] = useState({});
+  const [loading, setLoading] = useState(true);
+
+  const { firebase } = useContext(FirebaseContext);
 
   const user = {
     displayName: 'Payton',
@@ -15,7 +20,29 @@ function BrowseContainer() {
 
   return profile.displayName ? (
     <>
-      <div>Browse Container</div>
+      <Header src="joker1" dontShowOnSmallViewPort>
+        <Header.Frame>
+          <Header.Group>
+            <Header.Logo
+              to={ROUTES.HOME}
+              src="/images/misc/logo.svg"
+              alt="Netflix"
+            />
+            <Header.TextLink
+              active={category === 'series' ? 'true' : 'false'}
+              onClick={() => setCategory('series')}
+            >
+              Series
+            </Header.TextLink>
+            <Header.TextLink
+              active={category === 'films' ? 'true' : 'false'}
+              onClick={() => setCategory('series')}
+            >
+              Films
+            </Header.TextLink>
+          </Header.Group>
+        </Header.Frame>
+      </Header>
       <FooterContainer />
     </>
   ) : (
