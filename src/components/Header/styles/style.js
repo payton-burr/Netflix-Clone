f893d9f045e8537d2939cc221be034e2398701b3
Payton <greatpayton@gmail.com>
2021-02-27 14:41:36 -0700

    added styles to Header components

--- src/components/Header/styles/style.js
+++ src/components/Header/styles/style.js
@@ -98,8 +98,42 @@ export const ButtonLink = styled(ReactRouterLink)`
   }
 `;
 
-export const Feature = styled.div``;
+export const Feature = styled(Container)`
+  padding: 150px 0 500px 0;
+  flex-direction: column;
+  align-items: normal;
+  width: 50%;
 
-export const FeatureCallOut = styled.div``;
+  @media (max-width: 1100px) {
+    display: none;
+  }
+`;
 
-export const PlayButton = styled.button``;
+export const FeatureCallOut = styled.h2`
+  color: #ffffff;
+  font-size: 50px;
+  line-height: normal;
+  font-weight: bold;
+  text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.45);
+  margin: 0;
+`;
+
+export const PlayButton = styled.button`
+  box-shadow: 0 0.6vw 1vw -0.4vw rgba(0, 0, 0, 0.35);
+  background-color: #e6e6e6;
+  color: #000000;
+  border-width: 0;
+  padding: 10px 20px;
+  border-radius: 5px;
+  max-width: 130px;
+  font-weight: bold;
+  font-size: 20px;
+  margin-top: 10px;
+  cursor: pointer;
+  transition: background-color 0.5s ease;
+
+  &:hover {
+    background-color: #ff1e1e;
+    color: #ffffff;
+  }
+`;
