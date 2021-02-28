e2aa33d29d7af72d58e8e6b6dcf636b5db68307e
Payton <greatpayton@gmail.com>
2021-02-27 18:28:54 -0700

    imported films and series to be used later

--- src/pages/Browse.js
+++ src/pages/Browse.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import BrowseContainer from '../containers/BrowseContainer';
+import { useContent } from '../hooks';
 
 function Browse() {
+  const { series } = useContent('series');
+  const { films } = useContent('films');
+
+  console.log(series);
+  console.log(films);
   return <BrowseContainer />;
 }
 
