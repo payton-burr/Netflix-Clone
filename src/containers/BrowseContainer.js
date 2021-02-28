0a30720890a918f3bc3a8eb48d2cc95c6f98ab0f
Payton <greatpayton@gmail.com>
2021-02-27 23:13:16 -0700

    added Card component skeleton to BrowseContainer

--- src/containers/BrowseContainer.js
+++ src/containers/BrowseContainer.js
@@ -1,16 +1,18 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable prettier/prettier */
 import React, { useState, useContext, useEffect } from 'react';
-import { Header, Loading } from '../components';
+import { Card, Header, Loading } from '../components';
 import FirebaseContext from '../context/firebase';
 import { SelectProfileContainer } from './SelectProfileContainer';
 import { FooterContainer } from './FooterContainer';
 import * as ROUTES from '../constants/routes';
 
-function BrowseContainer() {
+function BrowseContainer({ slides }) {
   const [category, setCategory] = useState('series');
   const [profile, setProfile] = useState({});
   const [loading, setLoading] = useState(true);
   const [searchTerm, setSearchTerm] = useState('');
+  const [slideRows, setSlideRows] = useState([]);
 
   const { firebase } = useContext(FirebaseContext);
 
@@ -24,6 +26,10 @@ function BrowseContainer() {
       setLoading(false);
     }, 3000);
   }, [user]);
+
+  useEffect(() => {
+    setSlideRows(slides[category]);
+  }, [slides, category]);
 
   return profile.displayName ? (
     <>
@@ -84,6 +90,25 @@ function BrowseContainer() {
           <Header.PlayButton>Play</Header.PlayButton>
         </Header.Feature>
       </Header>
+
+      <Card.Group>
+        {slideRows.map(slideItem) => (
+          <Card key={`${category}-${slideItem.title.toLowerCase()}}`}>
+            <Card.Title>{slideItem.title}</Card.Title>
+            <Card.Entities>
+              {slideItem.data.map(item => (
+                <Card.Item key={item.docId} item={item}>
+                  <Card.Image src={`/images/${category}/${item.genre}/${item.slug}/small.jpg`}/>
+                  <Card.Meta>
+                    <Card.SubTitle>{item.title}</Card.SubTitle>
+                    <Card.Text>${item.description}</Card.Text>
+                  </Card.Meta>
+                </Card.Item>
+              ))}
+            </Card.Entities>
+          </Card>
+        )}
+      </Card.Group>
       <FooterContainer />
     </>
   ) : (
