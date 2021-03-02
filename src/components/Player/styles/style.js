e1bd17e74209b7ab67dfd3109ef726ebe60d8ed7
Payton <greatpayton@gmail.com>
2021-03-01 18:46:29 -0700

    added styles to Player

--- src/components/Player/styles/style.js
+++ src/components/Player/styles/style.js
@@ -2,8 +2,47 @@ import styled from 'styled-components';
 
 export const Container = styled.div``;
 
-export const Overlay = styled.div``;
+export const Overlay = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  margin: 0 20px;
+`;
 
-export const Inner = styled.div``;
+export const Inner = styled.div`
+  position: relative;
+  width: 100%;
+  max-width: 900px;
+  margin: auto;
 
-export const Button = styled.button``;
+  video {
+    width: 100%;
+    height: 100%;
+  }
+`;
+
+export const Button = styled.button`
+  background-color: #e50914;
+  border-color: #ff0a16;
+  width: 115px;
+  height: 45px;
+  text-transform: uppercase;
+  font-weight: bold;
+  color: #ffffff;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding-left: 0;
+
+  &:hover {
+    transform: scale(1.05);
+    background-color: #ff0a16;
+  }
+`;
